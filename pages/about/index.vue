f1b4219f538b279bf92eabce32d6f8eafbbb5a40
Abdulaziz Almohsen <abdulaziz@exeque.co>
2023-03-10 18:38:28 +0300

    policy

---
+++ pages/about/index.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <div
+      class="float-left relative py-0 px-4 w-full leading-4 cursor-default text-neutral-800"
+      id="terms-of-use"
+      data-windy-selected="64EL3b0bEwKXg6TOueqA2"
+      style="min-height: 1px"
+    >
+      <h1>ChatDrop Terms of Service</h1>
+      <br />
+      <small class="text-muted">Last modified: March 4, 2023</small>
+      <h2>Acceptance of the Terms of Service</h2>
+      <br />
+      <p>
+        These terms of service (
+        <strong style="font-weight: bold">"Agreement"</strong>) are entered into
+        by and between you and BRIDESVIEW, INC. DBA ChatDrop, a Delaware
+        Corporation, ( <strong style="font-weight: bold">"ChatDrop"</strong>,
+        <strong style="font-weight: bold">"We"</strong>,
+        <strong style="font-weight: bold">"Us"</strong>,
+        <strong style="font-weight: bold">"Our"</strong>). The following terms
+        and conditions govern your access to and use of (1) www.ChatDropapp.com
+        including any content, functionality, and services offered on or through
+        www.ChatDropapp.com the (
+        <strong style="font-weight: bold">"the Website"</strong>), (2) your
+        access, use, or purchase of the ChatDrop APP including any content,
+        functionality, and services offered on or through the ChatDrop APP (
+        <strong style="font-weight: bold">"the App"</strong>), and (3) your
+        access, use, or purchase of the ChatDrop services. The Website, the App,
+        and the Service Platforms are collectively referred to as the
+        <strong style="font-weight: bold">"Services"</strong>. Unless otherwise
+        indicated (see Acceptable and Prohibited Uses), this Agreement applies
+        to Self-Service Services.
+      </p>
+      <br />
+      <p>
+        Please read this Agreement carefully before you start to access,
+        purchase, or use the Self-Service Services. By accessing or using the
+        Services, you accept and agree to be bound and abide by this Agreement
+        and our Privacy Policy, incorporated herein by reference. If you do not
+        want to agree to this Agreement or the Privacy Policy, you must not
+        access or use the Services.
+      </p>
+      <br />
+      <p>
+        The Services are offered and available to users who are over the age of
+        18, have never been removed from the service, and whose registration and
+        use is in compliance with any and all applicable federal, state, local,
+        or international law or regulation (including, without limitation, any
+        laws regarding the export of data or software to and from the US or
+        other countries) (“Laws”). By accessing and using this Service, you
+        represent and warrant that you meet all of the foregoing eligibility
+        requirements. If you do not meet all of these requirements, you must not
+        access or use the Services.
+      </p>
+      <br />
+      <p>
+        <strong>
+          PLEASE NOTE: THIS AGREEMENT GOVERNS HOW DISPUTES BETWEEN YOU AND
+          ChatDrop CAN BE RESOLVED. IT CONTAINS A BINDING AND FINAL ARBITRATION
+          PROVISION AND CLASS ACTION WAIVER. PLEASE READ CAREFULLY AS IT AFFECTS
+          YOUR LEGAL RIGHTS, INCLUDING, IF APPLICABLE, YOUR RIGHT TO OPT OUT OF
+          ARBITRATION.
+        </strong>
+      </p>
+      <br />
+      <h2>Changes to the Agreement</h2>
+      <p>
+        We may revise and update this Agreement from time to time in our sole
+        discretion. All changes are effective immediately when we post them, and
+        apply to all access to and use of the Services thereafter. We will
+        notify you by contacting you through the email address you provide when
+        accessing or registering for the Services of any material changes to
+        this Agreement.
+      </p>
+      <br />
+      <p>
+        Your continued use of the Services following the posting of the revised
+        Agreement means that you accept and agree to the changes. You are
+        expected to check this page to remain aware of any changes, as they are
+        binding on you.
+      </p>
+      <br />
+      <p class="font-bold">1. LICENSE</p>
+      ChatDrop grants User a limited, non-exclusive, non-transferable, revocable
+      license to use the Application for personal, non-commercial use on any
+      device owned or controlled by User, in accordance with the terms and
+      conditions of this Agreement. 2.
+      <p class="font-bold">1. RESTRICTIONS</p>
+      User shall not: a. copy, modify, or distribute the Application in any
+      manner; b. use the Application for any illegal purpose; c. reverse
+      engineer, decompile, or disassemble the Application; d. attempt to gain
+      unauthorized access to the Application; e. use the Application to harass,
+      abuse, threaten, or harm any person; f. post or upload objectionable
+      content, including but not limited to content that is defamatory, obscene,
+      pornographic, or discriminatory; g. engage in any activity that interferes
+      with or disrupts the Application or its servers; h. use the Application in
+      any way that violates the rights of any third party; or i. use the
+      Application in any way that is contrary to the intended use of the
+      Application. 3.
+      <p class="font-bold">1. User Content</p>
+      User retains ownership of all content posted or uploaded to the
+      Application. By posting or uploading content, User grants ChatDrop a
+      non-exclusive, perpetual, worldwide, royalty-free license to use,
+      reproduce, distribute, display, and modify the content for the purposes of
+      operating and improving the Application. 4.
+      <p class="font-bold">Termination</p>
+      ChatDrop may terminate this Agreement at any time, with or without cause,
+      without liability to User. Upon termination, User must cease all use of
+      the Application and destroy all copies of the Application in User's
+      possession. 5.
+      <p class="font-bold">WARRANTY DISCLAIMER</p>
+      THE APPLICATION IS PROVIDED "AS IS" AND WITHOUT WARRANTY OF ANY KIND,
+      EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+      WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+      CHATDROP DOES NOT WARRANT THAT THE APPLICATION WILL MEET USER'S
+      REQUIREMENTS OR THAT THE OPERATION OF THE APPLICATION WILL BE
+      UNINTERRUPTED OR ERROR-FREE. 6.
+      <p class="font-bold">LIMITATION OF LIABILITY</p>
+      IN NO EVENT SHALL CHATDROP BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+      SPECIAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF OR RELATED TO THE USE OR
+      INABILITY TO USE THE APPLICATION, EVEN IF CHATDROP HAS BEEN ADVISED OF THE
+      POSSIBILITY OF SUCH DAMAGES. 7.
+      <p class="font-bold">INDEMNIFICATION</p>
+      User shall indemnify, defend, and hold ChatDrop harmless from any and all
+      claims, damages, expenses, or liabilities arising out of or related to
+      User's use of the Application, including but not limited to User's breach
+      of this Agreement.
+      <p class="font-bold">MODIFICATIONS TO AGREEMENT</p>
+      ChatDrop reserves the right to modify this Agreement at any time. User's
+      continued use of the Application after any such modification shall
+      constitute User's acceptance of the modified Agreement. 9.
+      <p class="font-bold">GENERAL PROVISIONS</p>
+      This Agreement shall be governed by and construed in accordance with the
+      laws of the state of California, without giving effect to its conflict of
+      laws principles. This Agreement constitutes the entire agreement between
+      the parties with respect to the subject matter hereof and supersedes all
+      prior or contemporaneous communications and proposals, whether oral or
+      written, between the parties. If any provision of this Agreement is held
+      to be invalid or unenforceable, the remaining provisions shall remain in
+      full force and effect. The failure of ChatDrop to enforce any right or
+      provision of this Agreement shall not be deemed a waiver of such right or
+      provision.
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
