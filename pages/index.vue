1cb31fd35f15e71be7395ad2db61c115087e60f3
Abdulaziz Almohsen <abdulaziz@exeque.co>
2023-03-12 18:53:33 +0300

    footer

--- pages/index.vue
+++ pages/index.vue
@@ -22,12 +22,7 @@
         </a>
       </div>
       <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-          <span class="sr-only">Open main menu</span>
-          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
+       
       </div>
      
      
@@ -64,6 +59,25 @@
           </defs>
         </svg>
       </div>
+         <footer aria-label="Site Footer" class="bg-white">
+  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+    <div
+      class="flex flex-col items-center gap-4 rounded-lg bg-indigo-600 p-6 shadow-lg sm:flex-row sm:justify-between"
+    >
+      <strong class="text-white">
+        For support: abdulazizalmohsen92@gmail.com
+      </strong>
+
+      
+    </div>
+
+    
+
+    
+  </div>
+</footer>
+
+
     </div>
   </main>
 </div>
